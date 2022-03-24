7b8918de18acbe3bf402ecc65d393dc54a76b4fe
f1a2i3z4a5n6r7a8h9m9a0n <101980522+f1a2i3z4a5n6r7a8h9m9a0n@users.noreply.github.com>
2022-03-25 04:26:30 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageDraw, ImageFont
+
+import math
+
+chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
+charArray = list(chars)
+charLength = len(charArray)
+interval = charLength/256
+
+def getChar(inputInt):
+    return charArray[math.floor(inputInt*interval)]
+
+text_file = open("Output.txt", "w")
+
+im = Image.open("cran.png")
+width, height = im.size
+pix = im.load()
+
+for i in range(height):
+    for j in range(width):
+        r, g, b = pix[j, i]
+        h = int(r/3 + g/3 + b/3)
+        pix[j, i] = (h, h, h)
+        text_file.write(getChar(h))
+
+    text_file.write('\n')
+
+im.save('output.png')
